47377f0ed8f6597c4d40165636914fe85c2bdfe2
gilmijar <65573701+gilmijar@users.noreply.github.com>
2025-11-23 21:33:08 +0100

    Initializing (#1)
    * automatic intitialisation files
    
    * renamed folder, added first sources
    
    * first model done
    
    ---------
    
    Co-authored-by: gilmijar+dbtcloud@gmail.com <gilmijar+dbtcloud@gmail.com>

---
+++ models/tech_week/int_rental.sql
@@ -0,0 +1,26 @@
+with
+    rental as (
+        select *
+        from {{ source('landing', 'rental') }}
+    )
+    , payment as (
+        select *
+        from {{ source('landing', 'payment') }}
+    )
+
+
+select 
+    rental_id
+    , rental.rental_rate
+    , rental.customer_id
+    , rental.inventory_id
+    , rental.staff_id
+    , rental.rental_date
+    , rental.payment_deadline
+    , rental.create_date
+    , payment.payment_id
+    , payment.amount
+    , payment.payment_date
+    
+from rental
+left join payment using (rental_id)
