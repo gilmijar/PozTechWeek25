4b1b4127aa2528a13c6bbb108521330ba847496c
gilmijar <65573701+gilmijar@users.noreply.github.com>
2026-06-18 09:54:08 +0200

    build customer intermediate (#8)
    * build customer intermediate
    
    * fix column alias
    
    ---------
    
    Co-authored-by: Ilia Gilmijarow <gilmijar+dbtcloud@gmail.com>

---
+++ models/tech_week/int_customer.sql
@@ -0,0 +1,40 @@
+with country as (
+    select
+        country_id
+        , country
+    from {{ source("landing", "country") }}
+)
+
+, city as (
+    select
+        city_id
+        , city
+        , country_id
+    from {{ source("landing", "city") }}
+)
+
+, address as (
+    select
+        address_id
+        , address || address2 as customer_address
+        , postal_code
+        , city_id
+    from {{ source("landing", "address") }}
+)
+
+, customer as (
+    select
+        customer_id
+        , first_name
+        , last_name
+        , email
+        , birth_date
+        , address_id
+    from {{ source("landing", "customer") }}
+)
+
+SELECT *
+from customer
+join address using (address_id)
+join city using (city_id)
+join country using (country_id)
